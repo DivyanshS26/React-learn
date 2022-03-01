bc69020d89ec3a3254ccd762ef1ae8f50025a311
Divyansh <divyanshsahu25@gmail.com>
2022-03-01 22:07:50 +0530

    props-2

--- props/src/App.js
+++ props/src/App.js
@@ -1,12 +1,36 @@
 import Contact from "./components/Contact";
+import ct1 from "./components/Images/cat1.jpg";
+import ct2 from "./components/Images/cat2.jpg";
+import ct3 from "./components/Images/cat3.jpg";
+import ct4 from "./components/Images/cat4.jpg";
 
 function App() {
   return (
     <div className="App">
-      <Contact/>
-      <Contact/>
-      <Contact/>
-      <Contact/>
+      <Contact
+        img={ct1}
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        eml="mr.whiskaz@catnap.meow"
+      />
+      <Contact
+        img={ct2}
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        eml="fluff@me.com"
+      />
+      <Contact
+        img={ct3}
+        name="Felix"
+        phone="(212) 555-4567"
+        eml="thecat@hotmail.com"
+      />
+      <Contact
+        img={ct4}
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        eml="pumpkin@scrimba.com"
+      />
     </div>
   );
 }
--- props/src/components/Contact.js
+++ props/src/components/Contact.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-const Contact = () => {
+const Contact = (props) => {
   return (
     <div className="contact-card">
-      <img src="./images/mr-whiskerson.png" />
-      <h3>Mr. Whiskerson</h3>
+      <img src={props.img} alt=""/>
+      <h3>{props.name}</h3>
       <div className="info-group">
-        <img src="./images/phone-icon.png" />
-        <p>(212) 555-1234</p>
+        {/* <img src="./images/phone-icon.png" /> */}
+        <p>{props.phone}</p>
       </div>
       <div className="info-group">
-        <img src="./images/mail-icon.png" />
-        <p>mr.whiskaz@catnap.meow</p>
+        {/* <img src="./images/mail-icon.png" /> */}
+        <p>{props.eml}</p>
       </div>
     </div>
   );
