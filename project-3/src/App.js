f9346c4fae674be695cc3db57c4b89ef07a84335
Divyansh <divyanshsahu25@gmail.com>
2022-03-11 18:55:49 +0530

    airbnb

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -4,7 +4,6 @@ import Hero from "./components/Hero";
 import Card from "./components/Card";
 import Data from "./Data";
 
-// <Hero />
 function App() {
   const DataElements = Data.map((data) => {
     return <Card key={data.id} {...data} />;
@@ -12,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
       <section className="crd-list">{DataElements}</section>
     </div>
   );
