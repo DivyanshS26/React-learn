b9b0037f4e52314df2d8db8b7b6ed1a7415de2f6
Divyansh <divyanshsahu25@gmail.com>
2022-03-26 18:01:45 +0530

    state-intro

--- Basic/b1/app.js
+++ Basic/b1/app.js
@@ -156,7 +156,7 @@ ReactDOM.render(
     document.getElementById("root")
 
 )
-*/
+
 
 function App(){
   const firstname = "david";
@@ -181,6 +181,64 @@ function App(){
     <div>
       <h1>Good {timeOfDay}!</h1>
       <h1>Hello {firstname} {lastname} !</h1>
+    </div>
+  )
+}
+
+function greeting(name) {
+    const date = new Date()
+    const hours = date.getHours()
+    
+    let timeOfDay
+    if(hours >= 4 && hours < 12) {
+        timeOfDay = "morning"
+    } else if(hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon"
+    } else if(hours >= 17 && hours < 20) {
+        timeOfDay = "evening"
+    } else {
+        timeOfDay = "night"
+    }
+    
+    return `Good ${timeOfDay}, ${name}!`
+}
+
+1. How would you describe the concept of "state"?
+A way for React to remember saved values from within a component.
+This is similar to declaring variables from within a component,
+with a few added bonuses (which we'll get to later)
+
+
+2. When would you want to use props instead of state?
+Anytime you want to pass data into a component so that
+component can determine what will get displayed on the
+screen.
+
+
+3. When would you want to use state instead of props?
+Anytime you want a component to maintain some values from
+within the component. (And "remember" those values even
+when React re-renders the component).
+
+
+4. What does "immutable" mean? Are props immutable? Is state immutable?
+Unchanging. Props are immutable. State is mutable.
+
+*/
+
+function App(){
+  function HandleClick(){
+    thingArray.push(`Thing ${thingArray.length + 1}`)
+    console.log(thingArray);
+  }
+  const thingArray = ["Thing 1", "Thing 2"];
+  const thingData = thingArray.map(data => <p key={data} >{data}</p>)
+
+
+  return(
+    <div>
+      <button onClick={HandleClick}>Add Item</button>
+      {thingData}
     </div>
   )
 }
