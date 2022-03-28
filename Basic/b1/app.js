1785f5748be86c0457efa9537f4911e027cb8faf
Divyansh <divyanshsahu25@gmail.com>
2022-03-28 19:37:34 +0530

    useState

--- Basic/b1/app.js
+++ Basic/b1/app.js
@@ -1,4 +1,3 @@
-
 /*---------------reactDOM syntax------------
 ReactDOM.render(<ul><li>Jod</li><li>is Back</li></ul>, document.getElementById("root"))
 --------------composeable-------------------
@@ -224,7 +223,7 @@ when React re-renders the component).
 4. What does "immutable" mean? Are props immutable? Is state immutable?
 Unchanging. Props are immutable. State is mutable.
 
-*/
+
 
 function App(){
   function HandleClick(){
@@ -243,4 +242,124 @@ function App(){
   )
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+//----------State-------------------
+function App() {
+
+  const [isImportant, setIsImportant] = React.useState("Yes")
+
+  function handleClick(){
+    return setIsImportant("No");
+  }
+
+  return (
+    <div className="state">
+      <h1 className="state--title">Is state important to know?</h1>
+      <div onClick={handleClick} className="state--value">
+        <h1>{isImportant}</h1>
+      </div>
+    </div>
+  );
+}
+
+1. You have 2 options for what you can pass in to a
+   state setter function (e.g. `setCount`). What are they?
+   
+a. New value of state (setCount(42))
+b. Callback function - whatever the callback function 
+   returns === new value of state
+
+
+2. When would you want to pass the first option (from answer
+   above) to the state setter function?
+Whenever you don't need the previous value of state to determine
+what the new value of state should be.
+
+
+3. When would you want to pass the second option (from answer
+   above) to the state setter function?
+Whenever you DO need the previous value to determine the new value
+
+
+
+function App() {
+  
+  const [count , setCount] = React.useState(0);
+
+  /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+     
+
+  function add(){
+    setCount(prevCount => prevCount + 1);
+  }
+ 
+  function minus(){
+    setCount(prevCount => prevCount - 1)
+  }
+
+  return (
+    <div className="counter">
+        <button className="counter--minus" onClick={minus} >–</button>
+        <div className="counter--count">
+            <h1>{count}</h1>
+        </div>
+        <button className="counter--plus" onClick={add} >+</button>
+    </div>
+  );
+}
+
+
+function App() {
+  
+  const [isGoingOut, setIsGoingOut] = React.useState(true)
+
+  function handleClick() {
+    setIsGoingOut(prevState => !prevState)
+    //   if(prevState === true){
+    //     return false
+    //   }else{
+    //     return true
+    //   }
+    // }
+    
+  }
+  
+  return (
+      <div className="state">
+          <h1 className="state--title">Do I feel like going out tonight?</h1>
+          <div className="state--value" onClick={handleClick} >
+              <h1>{isGoingOut ? "Yes" : "No"}</h1>
+          </div>
+      </div>
+  )
+}
+*/
+//Things 2
+function App(){
+  
+  
+  const [thingArray, setThingArray] = React.useState(["Thing 1", "Thing 2"]);
+
+  function addItem() {
+    setThingArray(prevThingArray => [...prevThingArray, `Thing ${prevThingArray.length + 1}`])
+  }
+
+  const thingElement = thingArray.map(things => <p key={things}>{things}</p>)
+
+  return(
+    <div>
+      <button onClick={addItem}>Add Item</button>
+      {thingElement}
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import memesData from '../memesData';
 
 const Meme = () => {
+
+  const [memeImage, setMemeImage] = useState("");
 
   function getImage() {
     const memesArray = memesData.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
     const url = memesArray[randomNumber].url;
     // console.log(memesData.data.memes[Math.floor(Math.random() * memesData.data.memes.length)].url);
-    console.log(url);
+    setMemeImage(url);
   }
 
   return (
@@ -18,6 +20,7 @@ const Meme = () => {
             <input type="text" className='form-input' placeholder='bottom text'/>
             <button onClick={getImage} className='form-button'>Get a new meme image  🖼</button>
         </div>
+            <img src={memeImage} className="img" />
     </main>
   )
 }
